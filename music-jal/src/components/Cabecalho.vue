f7c48b7c863a023b3c8f1630fc04e1d2d432de72
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-03-02 14:08:51 -0300

    iniciando projeto em vue.js

---
+++ music-jal/src/components/Cabecalho.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <div class="information">
+      <img class="logo" src="../assets/logo.png" />
+      <h2>Music JAL</h2>
+   </div>
+    <div class="form">
+      <input
+        type="text"
+        v-model="input"
+        placeholder="Buscar por Artista ou Banda"
+      />
+      <button @click="sbmtbtn">buscar</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "CabecalhoItem",
+
+  props: {
+    name,
+  },
+  data: function () {
+    return {
+      input: "",
+    };
+  },
+
+  methods: {
+    sbmtbtn() {
+      console.log(this.input);
+    },
+  },
+};
+</script>
+
+<style>
+.logo {
+  width: 70px;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.form {
+  margin-right: 50px;
+}
+.information{
+  display: flex;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.information h2{
+  margin-left: 35px ;
+}
+</style>
---
+++ music-jal/src/components/Display.vue
@@ -0,0 +1,81 @@
+<template>
+  <main>
+    <div class="sidBar">
+      <div class="btnCategory" v-for="categoria in categorias" :key="categoria">
+        <p>{{ categoria }}</p>
+      </div>
+    </div>
+    <div class="cards">
+      <div class="card" v-for="a in albuns" :key="a.artistId">
+        <h2>{{ a.artistName }}</h2>
+        <p>{{ a.collectionName }}</p>
+        <img class="imgCard" alt="thumb" v-bind:src=" a.artworkUrl100 " />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import { dataAlbum, arrCategorias } from "../services/data";
+
+console.log("dataAlbum", dataAlbum);
+
+export default {
+  name: "DisplayItem",
+  data: function () {
+    return {
+      categorias: arrCategorias,
+      albuns: dataAlbum,
+    };
+  },
+  // mounted() {
+  //   const response = dataAlbum();
+  //   console.log('response', response);
+  //   // this.albuns => { [...response]};
+  //   console.log('this.albuns', this.albuns);
+  // }
+};
+</script>
+
+<style>
+main {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+}
+.btnCategory {
+  width: 170px;
+  border: solid 1px;
+  border-radius: 7px;
+  text-align: center;
+  padding: 6px;
+  margin-top: 1px;
+}
+.btnCategory p:hover {
+  width: 100%;
+  color: #fff;
+}
+.btnCategory:hover {
+  background-color: #1ea0c0;
+}
+.cards {
+  width: 80%;
+  display: flex;
+  flex-flow: wrap;
+  
+}
+.card {
+  width: 170px;
+  max-height: 270px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.imgCard {
+  width: 170px;
+}
+</style>
